faf85841a716fa6f2833b771163c91d719cc4b94
Jhoncarmack <165399569+Jhoncarmack@users.noreply.github.com>
2026-04-11 14:41:02 +0900

    feat: Add human choice input handler
    - 사용자가 가위바위보 선택을 입력할 수 있는 기능을 추가함
    - Rock, Paper, Scissors 입력값을 받아 선택 결과를 반환하도록 함

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let input = prompt("가위 바위 보 하겠습니다. 원하시는걸 내세요. ex) Rock / Paper / Scissors");
+function getHumanChoice() {
+   if (input === "Rock") {
+      return "Rock";
+   } else if (input === "Scissors") {
+      return "Scissors";
+   } else {
+      return "Paper";
+   }
+}
+
 function getComputerChoice() {
    if (Math.random() * 3 === 0) {
       return "ROCK";
@@ -7,4 +18,5 @@ function getComputerChoice() {
       return "Paper";
    }
 }
+getHumanChoice();
 getComputerChoice();
