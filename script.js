eed0fe886233ee049a1f3acedc4bcc3e88a8d10f
Jhoncarmack <165399569+Jhoncarmack@users.noreply.github.com>
2026-04-11 15:44:12 +0900

    feat: Add round logic and score tracking
     - 사용자의 입력을 대소문자와 관계없이 처리할 수 있도록 수정함
     - 사람과 컴퓨터의 가위바위보 선택을 비교하는 playRound 함수를 추가함
     - 라운드 결과에 따라 humanScore와 computerScore가 증가하도록 구현함
     - 사용자와 컴퓨터의 선택값을 받아 한 라운드를 실행하도록 연결함

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let input = prompt("가위 바위 보 하겠습니다. 원하시는걸 내세요. ex) Rock / Paper / Scissors");
 function getHumanChoice() {
-   if (input === "Rock") {
+   if (input.toUpperCase() === "ROCK") {
       return "Rock";
-   } else if (input === "Scissors") {
+   } else if (input.toUpperCase() === "SCISSORS") {
       return "Scissors";
    } else {
       return "Paper";
@@ -11,7 +11,7 @@ function getHumanChoice() {
 
 function getComputerChoice() {
    if (Math.random() * 3 === 0) {
-      return "ROCK";
+      return "Rock";
    } else if (Math.random() * 3 === 1) {
       return "Scissors";
    } else {
@@ -20,3 +20,57 @@ function getComputerChoice() {
 }
 getHumanChoice();
 getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+   if (humanChoice.toUpperCase() === "ROCK") {
+      if (computerChoice.toUpperCase() === "ROCK") {
+         console.log("비겼다");
+         console.log(computerSelection);
+      } else if (computerChoice.toUpperCase() === "SCISSORS") {
+         console.log("이겼다.");
+         console.log(computerSelection);
+         humanScore++;
+      } else {
+         console.log("졌다");
+         console.log(computerSelection);
+         computerScore++;
+      }
+   }
+   if (humanChoice.toUpperCase() === "SCISSORS") {
+      if (computerChoice.toUpperCase() === "SCISSORS") {
+         console.log("비겼다");
+         console.log(computerSelection);
+      } else if (computerChoice.toUpperCase() === "PAPER") {
+         console.log("이겼다");
+         console.log(computerSelection);
+         humanScore++;
+      } else {
+         console.log("졌다");
+         console.log(computerSelection);
+         computerScore++;
+      }
+   }
+   if (humanChoice.toUpperCase() === "PAPER") {
+      if (computerChoice.toUpperCase() === "PAPER") {
+         console.log("비겼다");
+         console.log(computerSelection);
+      } else if (computerChoice.toUpperCase() === "ROCK") {
+         console.log("이겼다.");
+         console.log(computerSelection);
+         humanScore++;
+      } else {
+         console.log("졌다");
+         console.log(computerSelection);
+         computerScore++;
+      }
+   }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
+// playRound(getHumanChoice(), getComputerChoice());
